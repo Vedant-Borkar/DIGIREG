5231fec03a956676228b2b3ceab3e4b3bbf16bde
Vedant-Borkar <122245126+Vedant-Borkar@users.noreply.github.com>
2024-02-18 22:47:40 +0530

    changes

--- digireg/src/App.js
+++ digireg/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Hello.js';
 
 function App() {
   return (
---
+++ digireg/src/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Hello() {
+  return (
+    <div className="hello">
+      Hello World
+    </div>
+  );
+}
+
+export default Hello;
